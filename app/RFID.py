d773d3d4d9d47f7ae803d997e6b498aba22baab3
syarifzapata <syarif.mathis@gmail.com>
2018-01-03 22:08:56 +0100

    correct typo

--- app/RFID.py
+++ app/RFID.py
@@ -1,5 +1,6 @@
 import pyfirmata
 from RPLCD import CharLCD
+import RPi.GPIO as GPIO
 
 board = pyfirmata.Arduino('/dev/ttyACM0')
 
@@ -7,7 +8,7 @@ DataPin_4 = board.get_pin('d:4:o')
 DataPin_5 = board.get_pin('d:5:o')
 DataPin_6 = board.get_pin('d:6:o')
 DataPin_7 = board.get_pin('d:7:o')
-PinRS = board.get_oin('d:2:o')
+PinRS = board.get_pin('d:2:o')
 PinE = board.get_pin('d:3:o')
 
 lcd = CharLCD(cols=16, rows=2, pin_rs=PinRS, pin_e=PinE, pins_data=[DataPin_4, DataPin_5, DataPin_6, DataPin_7], numbering_mode = GPIO.BCM)
@@ -22,3 +23,6 @@ tagsCount = 0
 tagID = ""
 successRead = false
 correctTag = false
+
+
+lcd.write_string('Hello Simon')
